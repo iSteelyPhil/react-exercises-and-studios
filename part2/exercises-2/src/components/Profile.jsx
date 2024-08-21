43cffafdecafa67d42f61d5396692b3e8ea6e8c1
Phillip <philmography2010@gmail.com>
2024-08-20 21:37:33 -0500

    ex 2 complete

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,26 @@
-import './styles.css';
-import Button from './Button';
+import "./styles.css";
+import Button from "./Button";
+import oceans from "./oceans.json";
 
+let listItem = oceans.map(ocean => (
+  <div
+    key={ocean.id}
+    className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}
+  >
+    <h1>{ocean.name}</h1>
+    <h2>
+      Fun Facts!
+      <ul>{ocean.fact1}</ul>
+      <ul>{ocean.fact2}</ul>
+      <ul>{ocean.fact3}</ul>
+    </h2>
+    <Button />
+    <img src={ocean.image} alt={ocean.name} className="img" />
+  </div>
+));
 
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
-   );
+function Profile() {
+  return <ul>{listItem}</ul>;
 }
 
 export default Profile;
