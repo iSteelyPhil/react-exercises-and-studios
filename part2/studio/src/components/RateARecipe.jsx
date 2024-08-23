5dbc64de68d9d08592569c6aa8232d3f6419fee1
Phil <philmography2010@gmail.com>
2024-08-22 20:25:14 -0500

    update

--- part2/studio/src/components/RateARecipe.jsx
+++ part2/studio/src/components/RateARecipe.jsx
@@ -1,9 +1,14 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
 function RateARecipe() {
+  function giveRating(props) {
+    return props.rating >= 1 && props.rating <= 5 ? (
+      <h3>{stars[props.rating - 1]}</h3>
+    ) : null;
+  }
   return stars;
 }
 
 export default RateARecipe;
 
-//conditional to render stars based on a number provided in App.js 
+//conditional to render stars based on a number provided in App.js
